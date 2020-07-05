393ed7c4c715bad3acbd0219d408a71602ca4ae1
ichiha-off <bsk.6510riku@gmail.com>
2020-07-05 02:33:44 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  get 'favorites/create'
+  get 'favorites/destroy'
+  root 'homes#top' 
+  get 'home/about' => 'homes#about'
+
+  devise_for :users
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :following, :followers
+     end
+  end
+  
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+end
