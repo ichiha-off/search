a4b3bc4ccaf8c68b2634a528d733fe94235d4ee9
ichiha-off <bsk.6510riku@gmail.com>
2020-07-05 07:45:31 +0000

    Fix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,48 @@
 class SearchesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def search
     @range = params[:range]
     search = params[:search]
     word = params[:word]
     if @range == '1'
-      @user = User.search(search,word)
+      @user = search_user(search,word)
+    elsif @range == '2'
+      @book = search_book(search,word)
+    elsif @range == '0'
+      redirect_back(fallback_location: root_path)
+      flash[:notice] = "「User」か「Book」を選んでください。"
+    end
+  end
+
+  private
+
+  def search_user(search,word)
+    if search == "forward_match"
+      User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      User.where("#{word}")
+    elsif search == "partial_match"
+      User.where("name LIKE?","%#{word}%")
     else
-      @book = Book.search(search,word)
+      User.all
+    end
+  end
+
+  def search_book(search,word)
+    if search == "forward_match"
+      Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      Book.where("title LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      Book.where("#{word}")
+    elsif search == "partial_match"
+      Book.where("title LIKE?","%#{word}%")
+    else
+      Book.all
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,18 +34,18 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
 
-  def self.search(search,word)
-    if search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "perfect_match"
-      @user = User.where("#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
-    else
-      @user = User.all
-    end
-  end
+  # def self.search(search,word)
+  #   if search == "forward_match"
+  #     User.where("name LIKE?","#{word}%")
+  #   elsif search == "backward_match"
+  #     User.where("name LIKE?","%#{word}")
+  #   elsif search == "perfect_match"
+  #     User.where("#{word}")
+  #   elsif search == "partial_match"
+  #     User.where("name LIKE?","%#{word}%")
+  #   else
+  #     User.all
+  #   end
+  # end
 
 end
